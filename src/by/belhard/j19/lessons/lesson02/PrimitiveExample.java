54ee120155bc99eabc3d93736760a93a99d794fb
Rozenberg <avangard.npaper@gmail.com>
2019-12-10 20:36:04 +0300

    init

---
+++ src/by/belhard/j19/lessons/lesson02/PrimitiveExample.java
@@ -0,0 +1,22 @@
+package by.belhard.j19.lessons.lesson02;
+
+// by.belhard.j19.lessons.lesson02.PrimitivesExample
+public class PrimitiveExample {
+
+    // psvm
+    public static void main(String[] args) {
+
+        /*short sh1 = 5;
+        int i = 100;
+        double d = 3.14;
+
+        System.out.println((sh1 - d) / i);
+*/
+        int i = 0;
+
+        System.out.println(i++);
+
+        System.out.println(++i);
+
+    }
+}
