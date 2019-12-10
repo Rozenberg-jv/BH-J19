1c12e6e82c06020d8639afabfd9389380e920f0a
Rozenberg <avangard.npaper@gmail.com>
2019-12-10 20:49:29 +0300

    2nd

--- src/by/belhard/j19/lessons/lesson02/PrimitiveExample.java
+++ src/by/belhard/j19/lessons/lesson02/PrimitiveExample.java
@@ -6,15 +6,15 @@ public class PrimitiveExample {
     // psvm
     public static void main(String[] args) {
 
-        /*short sh1 = 5;
+        /*short sh1 = 5; //
         int i = 100;
         double d = 3.14;
-
         System.out.println((sh1 - d) / i);
 */
         int i = 0;
 
         System.out.println(i++);
+
 
         System.out.println(++i);
 
