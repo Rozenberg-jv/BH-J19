927f9291c751fc57b63091f4c513f707dafc3255
Rozenberg-jv <avangard.npaper@gmail.com>
2019-12-13 16:02:01 +0300

    3rd prepared

---
+++ src/by/belhard/j19/lessons/lesson03/prepared/IfExample.java
@@ -0,0 +1,16 @@
+package by.belhard.j19.lessons.lesson03.prepared;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 7;
+
+		if (a != b)
+			System.out.println(a + " not equals " + b);
+
+
+	}
+
+}
