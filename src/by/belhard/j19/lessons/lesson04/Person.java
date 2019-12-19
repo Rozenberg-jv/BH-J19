63e98ca91ca1a0a3c1dc83eb6c2b0c1c35716b05
Rozenberg-jv <avangard.npaper@gmail.com>
2019-12-19 13:52:30 +0300

    fix

--- src/by/belhard/j19/lessons/lesson04/Person.java
+++ src/by/belhard/j19/lessons/lesson04/Person.java
@@ -2,63 +2,69 @@ package by.belhard.j19.lessons.lesson04;
 
 public class Person {
 
-    String name;
-    int age;
-    Country country;
-    Sex sex;
+	String name;
+	int age;
+	Country country;
+	Sex sex;
 
-    public Person(String name, int age, Country country, Sex sex) {
-        this.name = name;
-        this.age = age;
-        this.country = country;
-        this.sex = sex;
-    }
+	public Person(String name, int age, Country country, Sex sex) {
 
-    Person(String name) {
-        this.name = name;
-        this.age = 25;
-        this.sex = Sex.MALE;
-    }
+		this.name = name;
+		this.age = age;
+		this.country = country;
+		this.sex = sex;
+	}
 
-    /*
-     * Person Person(String name, int age, Country country, Sex sex) {
-     *
-     *   Person person = new Person();
-     *   person.name = name;
-     *   person.age = age;
-     *   ...
-     *
-     *   return person;
-     * }
-     * */
+	Person(String name) {
 
-    void walk() {
-//        System.out.println(name + " is walking");
-        walk(10);
-    }
+		this.name = name;
+		this.age = 25;
+		this.sex = Sex.MALE;
+	}
 
-    void walk(int minutes) {
-        System.out.println(name + " is walking for " + minutes + " minutes");
-    }
+	/*
+	 * Person Person(String name, int age, Country country, Sex sex) {
+	 *
+	 *   Person person = new Person();
+	 *   person.name = name;
+	 *   person.age = age;
+	 *   ...
+	 *
+	 *   return person;
+	 * }
+	 * */
 
-    void eat(String dish) {
-        if (dish.equals(""))
-            return;
+	void walk() {
+		//        System.out.println(name + " is walking");
+		walk(10);
+	}
 
-        System.out.println(name + " is eating " + dish + ". Om-nom-nom!");
-    }
+	void walk(int minutes) {
 
-    int growOld() {
-        return this.age;
-    }
+		System.out.println(name + " is walking for " + minutes + " minutes");
+	}
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", country=" + country +
-                ", sex=" + sex +
-                '}';
-    }
+	void eat(String dish) {
+
+		if (dish.equals(""))
+			return;
+
+		System.out.println(name + " is eating " + dish + ". Om-nom-nom!");
+	}
+
+	int growOld() {
+
+		return ++age;
+	}
+
+	@Override
+	public String toString() {
+
+		return "Person{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				", country=" + country +
+				", sex=" + sex +
+				'}';
+	}
 }
